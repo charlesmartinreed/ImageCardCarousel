930909711db99ff9d90b5ea94313dc6de309280e
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-29 16:31:58 -0600

    Add titles to image cards using CATextLayer.

--- ImageCardCarousel/ViewController.swift
+++ ImageCardCarousel/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     }
 
     //MARK:- Properties
+    let imageNames: [String] = ["Ajanta Caves", "Ellora Caves", "Agra Fort", "Taj Mahal", "Sun Temple", "Monuments at Mahabalipuram", "Kaziranga National Park"]
     
     let transformLayer = CATransformLayer()
     var currentAngle: CGFloat = 0
@@ -31,15 +32,18 @@ class ViewController: UIViewController {
         view.layer.addSublayer(transformLayer)
         
         //for loop to add images to the transformLayer
-        for i in 1...6 { //6 because we only have 6 images at the moment
-            addImageCard(name: "\(i)")
+        for i in 1..<imageNames.count { //6 because we only have 6 images at the moment
+            addImageCard(name: "\(i)", title: String(imageNames[i]))
         }
         
         turnCarousel() //image cards will be arranged in a circle
         
         //MARK:- Pan Gesture recognizer
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.performPanAction(recognizer:)))
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.performTapAction(recognizer:)))
+        
         view.addGestureRecognizer(panGestureRecognizer)
+        view.addGestureRecognizer(tapGestureRecognizer)
         
     }
     
@@ -74,25 +78,42 @@ class ViewController: UIViewController {
         }
 }
     
-    fileprivate func addImageCard(name: String) {
+    fileprivate func addImageCard(name: String, title: String) {
         
         let imageCardSize = CGSize(width: 200, height: 300)
         
         //create the image Layer
         let imageLayer = CALayer()
+        imageLayer.name = title
         imageLayer.frame = CGRect(x: view.frame.size.width / 2 - imageCardSize.width / 2 , y: view.frame.size.height / 2 - imageCardSize.height / 2, width: imageCardSize.width, height: imageCardSize.height)
+        
+        //create the image title layer
+        let imageTitleLayer = CATextLayer()
+        //create a CATextLayer to serve as label
+        let imageTitleLayerWidth: CGFloat = 150
+        let imageTitleLayerHeight: CGFloat = 100
+        //imageTitleLayer.frame = CGRect(x: imageLayer.frame.width * 2 + imageTitleLayerWidth , y: imageLayer.frame.height / 2 + imageTitleLayerHeight, width: imageTitleLayerWidth, height: imageTitleLayerHeight)
+        imageTitleLayer.frame = CGRect(x: (imageLayer.bounds.width / 2) - (imageTitleLayerWidth / 2), y: imageLayer.bounds.height - 24, width: imageTitleLayerWidth, height: imageTitleLayerHeight)
+        
+        imageTitleLayer.string = imageLayer.name
+        imageTitleLayer.foregroundColor = UIColor.blue.cgColor
+        imageTitleLayer.contentsScale = UIScreen.main.scale //should fix the blurred text issue
+        imageTitleLayer.fontSize = 14
+        
+        imageLayer.addSublayer(imageTitleLayer)
         
         //the point around which our carousel will rotate, 0.5 is actually default
         imageLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
         //add the image(s) to our layer - note that we need to convert to a cgImage
         guard let imageCardImage = UIImage(named: name)?.cgImage else { return }
+        
         imageLayer.contents = imageCardImage
         imageLayer.contentsGravity = .resizeAspectFill //similar to contentMode with UIImageView
         imageLayer.masksToBounds = true
         
         //imageLayer properties
-        imageLayer.isDoubleSided = true //makes the backside visible when we're rotating our layer in 3D
+        imageLayer.isDoubleSided = false //makes the backside visible when we're rotating our layer in 3D
         imageLayer.borderColor = UIColor(white: 1, alpha: 0.5).cgColor
         imageLayer.borderWidth = 5
         imageLayer.cornerRadius = 10
@@ -101,7 +122,7 @@ class ViewController: UIViewController {
         transformLayer.addSublayer(imageLayer)
     }
 
-    //MARK:- Gesture recognizer helper method
+    //MARK:- Gesture recognizer helper methods
     @objc fileprivate func performPanAction(recognizer: UIPanGestureRecognizer) {
         //update carousel transform layout values
         
@@ -121,6 +142,45 @@ class ViewController: UIViewController {
         //update the values continuously while the finger is moving
         turnCarousel()
     }
+    
+    @objc fileprivate func performTapAction(recognizer: UITapGestureRecognizer) {
+        //center the card in question
+        
+        
+        //TESTING: print the name of the wonder depicted in the image
+        let touchLocation = recognizer.location(in: self.view)
+        //print(touchLocation)
+        
+        guard let sublayers = transformLayer.sublayers else { return }
+        //grab the imageLayer
+        for layer in sublayers {
+            if layer.frame.contains(touchLocation) {
+                guard let locationName = layer.name else { return }
+                print(locationName)
+            }
+        }
+    }
 
 }
 
+extension CATextLayer {
+    static let textLabel: CATextLayer = {
+       let label = CATextLayer()
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.foregroundColor = UIColor.white.cgColor
+        return label
+    }()
+}
+
+extension UIView {
+    func anchorBottomRight() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewTrailing = superview?.trailingAnchor {
+            trailingAnchor.constraint(equalTo: superviewTrailing, constant: -8)
+        }
+        
+        if let superviewBottom = superview?.bottomAnchor {
+            bottomAnchor.constraint(equalTo: superviewBottom, constant: -8)
+        }
+    }
+}
